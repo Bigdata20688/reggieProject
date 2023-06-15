9a18350810726206d64a868a90a18afed39a4083
pyh2544942967 <135930745+pyh2544942967@users.noreply.github.com>
2023-06-15 16:08:31 +0800

    庞宇航

---
+++ src/main/java/edu/czjt/reggie/mapper/OrderDetailMapper.java
@@ -0,0 +1,12 @@
+package edu.czjt.reggie.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import edu.czjt.reggie.entity.OrderDetail;
+import org.apache.ibatis.annotations.Mapper;
+/**
+ * 庞宇航  订单
+ */ /
+@Mapper
+public interface OrderDetailMapper extends BaseMapper<OrderDetail> {
+
+}
---
+++ src/main/java/edu/czjt/reggie/mapper/OrderMapper.java
@@ -0,0 +1,12 @@
+package edu.czjt.reggie.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import edu.czjt.reggie.entity.Orders;
+import org.apache.ibatis.annotations.Mapper;
+/**
+ * 庞宇航  订单
+ */ /
+@Mapper
+public interface OrderMapper extends BaseMapper<Orders> {
+
+}
