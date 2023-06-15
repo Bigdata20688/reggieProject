740a912641dc2bddb009ad9a4f0d3d686cce50b8
huxue <739206147@qq.com>
2023-06-15 15:30:25 +0800

    胡雪

---
+++ src/main/java/edu/czjt/reggie/service/AddressBookService.java
@@ -0,0 +1,14 @@
+package edu.czjt.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import edu.czjt.reggie.entity.AddressBook;
+/**
+ *
+ *作者：胡雪
+ *内容：地址模块
+ * 日期：2023/6/15
+ *
+ */
+public interface AddressBookService extends IService<AddressBook> {
+
+}
