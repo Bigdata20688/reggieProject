4cc2db1c6f3020348a4469d6226758955a134735
86183 <2209197499@qq.com>
2023-06-15 15:55:27 +0800

    张俊胜

---
+++ src/main/java/edu/czjt/reggie/service/CategoryService.java
@@ -0,0 +1,10 @@
+package edu.czjt.reggie.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import edu.czjt.reggie.entity.Category;
+
+/**#张俊胜
+
+ */
+public interface CategoryService extends IService<Category> {
+}
